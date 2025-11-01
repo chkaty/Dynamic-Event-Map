ba0cef872a051e9736e3b4031db709dc8230ff66
Katy Chen <katy.chen@mail.utoronto.ca>
2025-10-31 23:06:32 -0400

    feat: add comments functionality with socket.io integration

---
+++ api/socket.js
@@ -0,0 +1,25 @@
+let io = null;
+
+function init(server, opts = {}) {
+  const { Server } = require("socket.io");
+  io = new Server(server, {
+    cors: {
+      origin: opts.origin || `http://localhost:${process.env.FRONTEND_PORT || 3000}`,
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+    socket.on("disconnect", (reason) => console.log("Socket disconnected:", socket.id, reason));
+  });
+
+  return io;
+}
+
+function getIO() {
+  if (!io) throw new Error("Socket.io not initialized. Call init(server) first.");
+  return io;
+}
+
+module.exports = { init, getIO };
